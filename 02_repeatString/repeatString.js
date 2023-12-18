e0b6aaca63fceaab65fdf30fc78bb7c6654feaf8
KdenNijs <katinkadennijs@gmail.com>
2023-12-18 15:27:48 +0100

    Add solution to exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) return 'ERROR'
+    
+    let string2 = '';
 
+    for (; times; times--) {
+        string2 += string;
+    }
+
+    return string2;
 };
 
 // Do not edit below this line
