4720781514750421cfd2b15164ab1d683eb210a1
KdenNijs <katinkadennijs@gmail.com>
2023-12-18 16:36:00 +0100

    Try better solution to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,30 @@
-const removeFromArray = function() {
-    let array = arguments[0];
-
-    for (let i = 1; i < arguments.length; i++) {
-        let value = arguments[i]
-        // array.splice(array.indexOf(number), 1);
-        if (array.includes(value)) {
-            array.splice(array.indexOf(value), 1);
-        }
-    }
-    return array
+const removeFromArray = function(array, ...args) {
+    return array.filter(value => !args.includes(value));
 }
+
+// const removeFromArray = function() {
+//     const args = Array.from(arguments);
+//     console.log(args);
+    
+//     let array = args[0];
+
+//     for (value of args.slice(1)) {
+//         if (array.includes(value)) {
+//             array.splice(array.indexOf(value), 1);
+//         }
+//     }
+
+//     console.log(array);
+//     return array
+
+//     // for (let i = 1; i < arguments.length; i++) {
+//     //     let value = arguments[i]
+//     //     // array.splice(array.indexOf(number), 1);
+//     //     if (array.includes(value)) {
+//     //         array.splice(array.indexOf(value), 1);
+//     //     }
+//     // }
+//     // return array
+// }
 
 module.exports = removeFromArray;
