818de68e054302a7a4126640b890ac5eea505489
KdenNijs <katinkadennijs@gmail.com>
2023-12-18 16:02:11 +0100

    Solve more excercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
+    let array = arguments[0];
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        let value = arguments[i]
+        // array.splice(array.indexOf(number), 1);
+        if (array.includes(value)) {
+            array.splice(array.indexOf(value), 1);
+        }
+    }
+    return array
+}
 
-// Do not edit below this line
 module.exports = removeFromArray;
