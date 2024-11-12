428abfb54b2addff76f6af4553d316b65a2cabc8
KdenNijs <katinkadennijs@gmail.com>
2024-11-12 15:51:31 +0100

    Exercises palindromes finished.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    str = str.replace(/\W|_/g, '').toLowerCase();
+    let strReverse = str.split("").reverse().join("");
+    return str === strReverse;
 };
 
 // Do not edit below this line
